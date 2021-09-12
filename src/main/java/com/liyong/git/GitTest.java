338d369cb7f6ea5752cebc9cd55831753350612d
Stinber <stinber@outlook.com>
2021-09-12 23:21:48 +0800

    first commit

---
+++ src/main/java/com/liyong/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.liyong.git;
+
+/**
+ * @author LiYong
+ * @time 2021-09-12 23:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
