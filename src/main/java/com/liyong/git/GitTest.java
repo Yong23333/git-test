4f6bf45772e7a38142985af52edd86667c6c6976
Stinber <stinber@outlook.com>
2021-09-12 23:35:14 +0800

    hot-fix commit

--- src/main/java/com/liyong/git/GitTest.java
+++ src/main/java/com/liyong/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello World!");
         System.out.println("Hello Git!!!");
         System.out.println("Hello Hello!");
+        System.out.println("Hello World!");
     }
 }
